b513167ee5491d5c9237763da6c79358c80aeab1
Eric Afes <eriicafes@gmail.com>
2025-02-21 02:04:42 +0100

    Organize package exports

---
+++ .changeset/shy-bees-attend.md
@@ -0,0 +1,5 @@
+---
+"monarch-orm": minor
+---
+
+Use subpath exports for types and operators
--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,10 @@ export {
   InferInput,
   InferOutput,
 } from "./database";
-export { MonarchError, MonarchParseError } from "./errors";
+export { MonarchError } from "./errors";
 export { AnySchema, createSchema, Schema } from "./schema/schema";
 export { InferSchemaInput, InferSchemaOutput } from "./schema/type-helpers";
 export { Virtual, virtual } from "./schema/virtuals";
-export { array } from "./types/array";
-export { boolean } from "./types/boolean";
-export { createdAt, date, dateString, updatedAt } from "./types/date";
-export { literal } from "./types/literal";
-export { mixed } from "./types/mixed";
-export { number } from "./types/number";
-export { object } from "./types/object";
-export { objectId } from "./types/objectId";
-export { pipe } from "./types/pipe";
-export { record } from "./types/record";
-export { string } from "./types/string";
-export { taggedUnion } from "./types/tagged-union";
-export { tuple } from "./types/tuple";
-export { defaulted, MonarchType, nullable, optional, type } from "./types/type";
-export { InferTypeInput, InferTypeOutput } from "./types/type-helpers";
-export { union } from "./types/union";
 export {
   generateObjectId,
   isValidObjectId,
