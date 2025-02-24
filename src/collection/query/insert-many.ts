8af893d6257d666fcc05f14521d3a575ebd9763d
Prince Codes <princecodes247@gmail.com>
2025-02-24 22:17:26 +0100

    fix: update insertmany return type to match standard

--- src/collection/query/insert-many.ts
+++ src/collection/query/insert-many.ts
@@ -1,20 +1,29 @@
 import type {
   BulkWriteOptions,
-  InsertManyResult,
   Collection as MongoCollection,
-  OptionalUnlessRequiredId,
+  OptionalUnlessRequiredId
 } from "mongodb";
 import { type AnySchema, Schema } from "../../schema/schema";
 import type {
   InferSchemaData,
   InferSchemaInput,
+  InferSchemaOmit,
+  InferSchemaOutput,
 } from "../../schema/type-helpers";
-import { Query } from "./base";
+import type { Projection } from "../types/query-options";
+import { makeProjection } from "../utils/projection";
+import { Query, type QueryOutput } from "./base";
 
-export class InsertManyQuery<TSchema extends AnySchema> extends Query<
-  TSchema,
-  InsertManyResult<InferSchemaData<TSchema>>
-> {
+export class InsertManyQuery<
+TSchema extends AnySchema,
+TOutput = InferSchemaOutput<TSchema>[],
+TOmit extends ["omit" | "select", keyof any] = [
+  "omit",
+  InferSchemaOmit<TSchema>,
+],
+> extends Query<TSchema, QueryOutput<TOutput, TOmit>[]> {
+  private _projection: Projection<InferSchemaOutput<TSchema>>;
+
   constructor(
     protected _schema: TSchema,
     protected _collection: MongoCollection<InferSchemaData<TSchema>>,
@@ -23,6 +32,7 @@ export class InsertManyQuery<TSchema extends AnySchema> extends Query<
     private _options: BulkWriteOptions = {},
   ) {
     super(_schema, _collection, _readyPromise);
+    this._projection = makeProjection("omit", _schema.options.omit ?? {});
   }
 
   public options(options: BulkWriteOptions): this {
@@ -30,13 +40,23 @@ export class InsertManyQuery<TSchema extends AnySchema> extends Query<
     return this;
   }
 
-  public async exec(): Promise<InsertManyResult<InferSchemaData<TSchema>>> {
+  public async exec(): Promise<QueryOutput<TOutput, TOmit>[]> {
     await this._readyPromise;
     const data = this._data.map((data) => Schema.toData(this._schema, data));
     const res = await this._collection.insertMany(
       data as OptionalUnlessRequiredId<InferSchemaData<TSchema>>[],
       this._options,
     );
-    return res;
+    // return res;
+    return Object.values(res.insertedIds).map(id => {
+      const resValue = data.find(result => result?._id === id)
+      if(!resValue) return null;
+      return Schema.fromData(
+        this._schema,
+        resValue,
+        this._projection,
+        Object.keys(this._projection),
+      )
+  }) as QueryOutput<TOutput, TOmit>[];
   }
 }
