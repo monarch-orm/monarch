6f88300d9ad325a79fb2c380a0e1a0b7bb3ffccd
Prince Codes <princecodes247@gmail.com>
2025-12-16 17:06:55 +0100

    feat: implement distinct query

---
+++ src/collection/query/distinct.ts
@@ -0,0 +1,45 @@
+import type {
+  DistinctOptions,
+  Filter,
+  Collection as MongoCollection,
+} from "mongodb";
+import type { AnyRelations } from "../../relations/relations";
+import type { AnySchema } from "../../schema/schema";
+import type { InferSchemaData } from "../../schema/type-helpers";
+import { Query } from "./base";
+
+export class DistinctQuery<
+  TSchema extends AnySchema,
+  TDbRelations extends Record<string, AnyRelations>,
+  TOutput extends InferSchemaData<TSchema>,
+> extends Query<TSchema, TOutput> {
+  constructor(
+    protected _schema: TSchema,
+    protected _relations: TDbRelations,
+    protected _collection: MongoCollection<InferSchemaData<TSchema>>,
+    protected _readyPromise: Promise<void>,
+    private _filter: Filter<InferSchemaData<TSchema>>,
+    private _key: keyof TOutput,
+    private _options: DistinctOptions = {},
+  ) {
+    super(_schema, _collection, _readyPromise);
+  }
+
+  public options(options: DistinctOptions): this {
+    Object.assign(this._options, options);
+    return this;
+  }
+
+  public async exec(): Promise<TOutput> {
+    await this._readyPromise;
+
+    const res = await this._collection.distinct(
+      this._key as string,
+      this._filter,
+      {
+        ...this._options,
+      },
+    );
+    return res as any;
+  }
+}
